7d0fc32b2357884ccf67d290db53ddcfac2d31ce
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-02-21 15:33:17 +0100

    Add header file

---
+++ includes/minishell.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   minishell.h                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/02/21 14:38:31 by pmolnar       #+#    #+#                 */
+/*   Updated: 2023/02/21 15:29:21 by pmolnar       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# define PROMPT_MSG "minishell$ "
+
+char	*read_prompt(const char *prompt);
+
+#endif
