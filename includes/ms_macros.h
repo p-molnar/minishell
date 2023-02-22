f42b508cbe913d27b0384b5ea16616830a7da555
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-02-22 16:36:03 +0100

    Initial commit

---
+++ includes/ms_macros.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ms_macros.h                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/02/22 14:13:40 by pmolnar       #+#    #+#                 */
+/*   Updated: 2023/02/22 15:47:39 by pmolnar       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MS_MACROS_H
+# define MS_MACROS_H
+
+// operator symbols
+# define CHAR_PIPE "|"
+# define CHAR_AND '&'
+# define CHAR_SEMI ';'
+# define CHAR_LESS '<'
+# define CHAR_GREAT '>'
+# define CHAR_LPAREN '('
+# define CHAR_RPAREN ')'
+# define CHAR_DOLLAR '$'
+# define CHAR_BACKQUOTE '`'
+# define CHAR_BACKSLASH '\\'
+# define CHAR_DQUOTE '"'
+# define CHAR_QUOTE '\''
+# define CHAR_SPACE " "
+# define CHAR_TAB '\t'
+# define CHAR_NEWLINE '\n'
+
+#endif
