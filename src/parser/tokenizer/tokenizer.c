87a3efe0950f21d7ce6a5adb0618a4df2beb0603
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-02-22 16:37:07 +0100

    Initial commit

---
+++ src/parser/tokenizer/tokenizer.c
@@ -0,0 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   tokenizer.c                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/02/22 13:49:17 by pmolnar       #+#    #+#                 */
+/*   Updated: 2023/02/22 16:32:34 by pmolnar       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+#include <stdio.h>
+#include <ms_data_types.h>
+
+t_token	tokenizer(const char *prompt);
