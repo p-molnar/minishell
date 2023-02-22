7f9784bcc551231f703bd7125cafae1087e4b9d2
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-02-22 16:35:50 +0100

    Initial commit

---
+++ includes/ms_data_types.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ms_data_types.h                                    :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/02/22 15:01:52 by pmolnar       #+#    #+#                 */
+/*   Updated: 2023/02/22 16:15:38 by pmolnar       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MS_DATA_TYPES_H
+# define MS_DATA_TYPES_H
+
+typedef struct s_token
+{
+	char	*token;
+	int		token_type;
+}	t_token;
+
+#endif
