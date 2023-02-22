e4169bd22f4d243abce7f92a8783ec3a5e84315e
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-02-22 16:34:47 +0100

    Add function prototype

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,15 +6,21 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/02/21 14:38:31 by pmolnar       #+#    #+#                 */
-/*   Updated: 2023/02/21 15:29:21 by pmolnar       ########   odam.nl         */
+/*   Updated: 2023/02/22 16:15:15 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# include <ms_data_types.h>
+
 # define PROMPT_MSG "minishell$ "
 
+// character_reader.c
 char	*read_prompt(const char *prompt);
+
+// tokenizer.c
+t_token	tokenizer(const char *prompt);
 
 #endif
