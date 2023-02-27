75b0042eb0350a1156404247228cf01114f2c31a
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-02-27 17:22:00 +0100

    Add linked list, update main.c

--- src/main.c
+++ src/main.c
@@ -6,22 +6,27 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/02/20 13:47:47 by pmolnar       #+#    #+#                 */
-/*   Updated: 2023/02/21 15:29:09 by pmolnar       ########   odam.nl         */
+/*   Updated: 2023/02/27 16:31:55 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <ms_data_types.h>
 
 int	main(void)
 {
-	char	*prompt;
+	char			*prompt;
+	int				prog_running;
+	t_token_list	*tokens;
 
-	while (1)
+	prog_running = 1;
+	while (prog_running)
 	{
 		prompt = read_prompt(PROMPT_MSG);
-		printf("%s", prompt);
+		printf("original prompt |%s|\n", prompt);
+		tokens = tokenizer(prompt);
 		free(prompt);
 	}
 	return (0);
