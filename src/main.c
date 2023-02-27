b5feb1888e446dfd6524c41e235653f27c49fda9
jzaremba88 <jzaremba@student.codam.nl>
2023-02-27 16:34:38 +0100

    add signal handling and EOF

--- src/main.c
+++ src/main.c
@@ -6,23 +6,36 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/02/20 13:47:47 by pmolnar       #+#    #+#                 */
-/*   Updated: 2023/02/21 15:29:09 by pmolnar       ########   odam.nl         */
+/*   Updated: 2023/02/27 16:23:28 by jzaremba      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <termios.h>
+#include <readline/history.h>
+
+void	cleanup_before_exit(struct termios *original_termios)
+{
+	tcsetattr(0, 0, original_termios);
+	clear_history();
+}
 
 int	main(void)
 {
-	char	*prompt;
+	char			*prompt;
+	struct termios	original_termios;
 
+	setup_signal_handler(&original_termios);
 	while (1)
 	{
 		prompt = read_prompt(PROMPT_MSG);
+		if (!prompt)
+			break ;
 		printf("%s", prompt);
 		free(prompt);
 	}
+	cleanup_before_exit(&original_termios);
 	return (0);
 }
