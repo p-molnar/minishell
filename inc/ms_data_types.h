e2f380fbce0209efe1c8be6d677f8ca5bbe2d95c
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-02-27 23:50:03 +0100

    Fix linked list prototype

---
+++ inc/ms_data_types.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ms_data_types.h                                    :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/02/22 15:01:52 by pmolnar       #+#    #+#                 */
+/*   Updated: 2023/02/27 22:28:59 by pmolnar       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MS_DATA_TYPES_H
+# define MS_DATA_TYPES_H
+
+typedef int	t_token_type;
+
+typedef struct s_token_list
+{
+	char				*content;
+	t_token_type		type;
+	struct s_token_list	*next;	
+}	t_token_list;
+
+#endif
