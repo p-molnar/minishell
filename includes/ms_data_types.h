39dbdb37e98ec5117605e5084621651448b8a5de
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-02-27 17:14:54 +0100

    Change linked list prototype

--- includes/ms_data_types.h
+++ includes/ms_data_types.h
@@ -6,17 +6,20 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/02/22 15:01:52 by pmolnar       #+#    #+#                 */
-/*   Updated: 2023/02/22 16:15:38 by pmolnar       ########   odam.nl         */
+/*   Updated: 2023/02/27 16:23:14 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MS_DATA_TYPES_H
 # define MS_DATA_TYPES_H
 
-typedef struct s_token
+typedef int	t_token_type;
+
+typedef struct s_token_list
 {
-	char	*token;
-	int		token_type;
-}	t_token;
+	char			*content;
+	t_token_type	type;
+	t_token_list	*next;	
+}	t_token_list;
 
 #endif
