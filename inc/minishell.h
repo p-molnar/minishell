66a61bb9ac4a7436db668ca9843ffb82578cbd12
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-02-28 13:06:40 +0100

    Add function prototypes

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,7 +6,7 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/02/21 14:38:31 by pmolnar       #+#    #+#                 */
-/*   Updated: 2023/02/27 22:42:25 by pmolnar       ########   odam.nl         */
+/*   Updated: 2023/02/28 12:09:37 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,19 @@
 
 # define PROMPT_MSG "minishell$ "
 
-// character_reader.c
+//	character_reader.c
 char			*read_prompt(const char *prompt);
 
-// tokenizer.c
+//	tokenizer.c
 t_token_list	*tokenizer(const char *prompt);
 
-// token_list_util.c
+//	token_list_util.c
 t_token_list	*new_node(char *content, int token_type);
 void			add_node_last(t_token_list **list, t_token_list *node);
+void			free_list(t_token_list *list);
+
+//	token_classifier.c
+void	classify_tokens(t_token_list *list);
 
 
 #endif
