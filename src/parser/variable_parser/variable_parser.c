7ff9f26962ea455e68707425508cef01fb260996
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-03-08 16:40:26 +0100

    Add environment variable parser

--- src/parser/variable_parser/variable_parser.c
+++ src/parser/variable_parser/variable_parser.c
@@ -6,7 +6,7 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/08 09:15:33 by pmolnar       #+#    #+#                 */
-/*   Updated: 2023/03/08 15:27:41 by pmolnar       ########   odam.nl         */
+/*   Updated: 2023/03/08 16:37:11 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void	add_shell_var(t_var *new_var, t_list **list)
+void	add_var(t_var *new_var, t_list **list)
 {
 	t_list	*new_node;
 	t_list	*defined_var;
 	t_var	*old_var;
+	int		len;
 
 	defined_var = find_var_by_name(new_var, *list);
 	if (!defined_var)
@@ -33,7 +34,8 @@ void	add_shell_var(t_var *new_var, t_list **list)
 	else
 	{
 		old_var = defined_var->content;
-		if (!ft_strncmp(old_var->val, new_var->val, ft_strlen(new_var->val)))
+		len = ft_strlen(new_var->val);
+		if (!ft_strncmp(old_var->val, new_var->val, len + 1))
 			return ;
 		free(old_var->val);
 		old_var->val = new_var->val;
@@ -54,8 +56,25 @@ void	parse_shell_variable(t_shell_data *data)
 			if (!var)
 				return ;
 			extract_var_from_token(list->content, var);
-			add_shell_var(var, &data->shell_vars);
+			add_var(var, &data->shell_vars);
 		}
 		list = list->next;
+	}
+}
+
+void	parse_env_variable(char **env, t_list **list)
+{
+	char	*var_definition;
+	t_var	*var;
+
+	while (*env)
+	{
+		var = ft_calloc(1, sizeof(t_var));
+		if (!var)
+			return ;
+		var_definition = *env;
+		extract_var_from_token(var_definition, var);
+		add_var(var, list);
+		env++;
 	}
 }
