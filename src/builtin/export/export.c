0ea89df96255f4fc659c1a9d45b4d479969cd24e
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-03-13 15:51:56 +0100

    Add new file

---
+++ src/builtin/export/export.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   export.c                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/03/13 13:40:23 by pmolnar       #+#    #+#                 */
+/*   Updated: 2023/03/13 15:38:09 by pmolnar       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <minishell.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void	export(t_token_list *token, t_shell_data *data)
+{
+	t_var	tmp_var;
+	t_list	*var_node;
+
+	if (is_valid_var_definition(token->content))
+		extract_var_from_token(token->content, &tmp_var);
+	else
+		tmp_var.name = token->content;
+	var_node = find_var_by_name(&tmp_var, data->shell_vars);
+	if (!var_node)
+		return ;
+	add_var((t_var *) var_node->content, &data->env_vars);
+}
