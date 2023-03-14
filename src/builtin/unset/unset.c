e4a6598068c599ef9170df7f8ade77a8207323a7
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-03-14 10:59:14 +0100

    Fix for segfaulting `unset`
    If a variable was not defined (i.e. no present in the linked list) the
    function attempted to locate and free the corresponding node. Since the
    node does not exist, `find_var_by_name` returned NULL and `unset`
    attempted to free that pointer. The curernt commit fixes this issues.

--- src/builtin/unset/unset.c
+++ src/builtin/unset/unset.c
@@ -6,7 +6,7 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/13 16:17:39 by pmolnar       #+#    #+#                 */
-/*   Updated: 2023/03/14 09:57:43 by pmolnar       ########   odam.nl         */
+/*   Updated: 2023/03/14 10:57:59 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,6 +49,8 @@ void	unset(char *var_name, t_shell_data *data)
 	shell_ptr = find_var_by_name(&tmp_var, data->shell_vars);
 	if (shell_ptr)
 		del_node(&data->shell_vars, shell_ptr);
+	else
+		return ;
 	env_ptr = find_var_by_name(&tmp_var, data->env_vars);
 	if (env_ptr)
 		del_node(&data->env_vars, env_ptr);
