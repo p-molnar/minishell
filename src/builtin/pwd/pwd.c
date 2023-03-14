1d9c6c9373bce0c3bad4e14c91a91172d3a012cc
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-03-14 10:51:04 +0100

    Add new file

---
+++ src/builtin/pwd/pwd.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   pwd.c                                              :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/03/14 10:22:01 by pmolnar       #+#    #+#                 */
+/*   Updated: 2023/03/14 10:35:44 by pmolnar       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <minishell.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+void	pwd(t_shell_data *data)
+{
+	char	*cwd_path;
+	(void)	data;
+
+	cwd_path = getcwd(NULL, 0);
+	printf("%s\n", cwd_path);
+	free(cwd_path);
+}
