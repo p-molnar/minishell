6c6537d843ab23acb3732dada6cfa186d9ab7ddd
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-03-14 10:00:15 +0100

    Fix segfaulting `unset`
    The issue presents itself when a variable is defined in the shell scope but not
    in the environment, i.e. not exported. When the variable is unset, a pointer is
    assigned to point to the node to be removed. However this pointer was used for
    both the shell and the environment list, and hence once the node was removed
    form the shell scope, the pointer tried to find it in the environment as well.
    Since it was not defined in the environment scope, the pointer was updated to be
    NULL, and only after it meant to be freed. The progarm crashed, becaues the
    program attempted to free a NULL pointer. This is now fixed by dealing with the
    2 scopes under different vairables. That is, `shell_ptr` and `env_ptr`.

--- src/builtin/unset/unset.c
+++ src/builtin/unset/unset.c
@@ -6,7 +6,7 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/13 16:17:39 by pmolnar       #+#    #+#                 */
-/*   Updated: 2023/03/13 17:23:41 by pmolnar       ########   odam.nl         */
+/*   Updated: 2023/03/14 09:57:43 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #include <stdlib.h>
 #include <libft.h>
 
- void	del_node(t_list **list, t_list *node)
+void	del_node(t_list **list, t_list *node)
 {
 	t_list	*prev_node;
 	t_list	*tmp;
@@ -26,7 +26,7 @@
 		if (tmp == node)
 		{
 			if (prev_node == NULL)
-				*list = (tmp)->next;
+				*list = tmp->next;
 			else if (tmp->next == NULL)
 				prev_node->next = NULL;
 			else
@@ -40,19 +40,20 @@
 
 void	unset(char *var_name, t_shell_data *data)
 {
-	t_list	*node_ptr;
+	t_list	*env_ptr;
+	t_list	*shell_ptr;
 	t_var	tmp_var;
 
 	tmp_var.name = var_name;
 	tmp_var.val = "";
-	node_ptr = find_var_by_name(&tmp_var, data->shell_vars);
-	if (node_ptr)
-		del_node(&data->shell_vars, node_ptr);
-	node_ptr = find_var_by_name(&tmp_var, data->env_vars);
-	if (node_ptr)
-		del_node(&data->env_vars, node_ptr);
-	free(((t_var *) node_ptr->content)->name);
-	free(((t_var *) node_ptr->content)->val);
-	free(node_ptr->content);
-	free(node_ptr);
+	shell_ptr = find_var_by_name(&tmp_var, data->shell_vars);
+	if (shell_ptr)
+		del_node(&data->shell_vars, shell_ptr);
+	env_ptr = find_var_by_name(&tmp_var, data->env_vars);
+	if (env_ptr)
+		del_node(&data->env_vars, env_ptr);
+	free(((t_var *) shell_ptr->content)->name);
+	free(((t_var *) shell_ptr->content)->val);
+	free(shell_ptr->content);
+	free(shell_ptr);
 }
