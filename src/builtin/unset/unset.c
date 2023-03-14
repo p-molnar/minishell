8306edf72938af0a3c4e48237dd628107fd00e57
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-03-14 14:26:15 +0100

    Fix bug
    When attempting to unset default environment variables the program
    crashed. This is because no matter which scope the variable was defined,
    the program freed the shell variable. The current fix only frees the
    shell variable if it a user defined one. Otherwise it frees the
    environment one.

--- src/builtin/unset/unset.c
+++ src/builtin/unset/unset.c
@@ -6,7 +6,7 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/13 16:17:39 by pmolnar       #+#    #+#                 */
-/*   Updated: 2023/03/14 10:57:59 by pmolnar       ########   odam.nl         */
+/*   Updated: 2023/03/14 14:26:02 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,20 +42,27 @@ void	unset(char *var_name, t_shell_data *data)
 {
 	t_list	*env_ptr;
 	t_list	*shell_ptr;
-	t_var	tmp_var;
+	t_list	*ptr;
 
-	tmp_var.name = var_name;
-	tmp_var.val = "";
-	shell_ptr = find_var_by_name(&tmp_var, data->shell_vars);
-	if (shell_ptr)
+	shell_ptr = find_var_by_name(var_name, data->shell_vars);
+	env_ptr = find_var_by_name(var_name, data->env_vars);
+	ptr = shell_ptr;
+	if (shell_ptr && env_ptr)
+	{
 		del_node(&data->shell_vars, shell_ptr);
-	else
-		return ;
-	env_ptr = find_var_by_name(&tmp_var, data->env_vars);
-	if (env_ptr)
 		del_node(&data->env_vars, env_ptr);
-	free(((t_var *) shell_ptr->content)->name);
-	free(((t_var *) shell_ptr->content)->val);
-	free(shell_ptr->content);
-	free(shell_ptr);
+	}
+	else if (shell_ptr)
+		del_node(&data->shell_vars, shell_ptr);
+	else if (env_ptr)
+	{
+		del_node(&data->env_vars, env_ptr);
+		ptr = env_ptr;
+	}
+	if (!shell_ptr && !env_ptr)
+		return ;
+	free(((t_var *) ptr->content)->name);
+	free(((t_var *) ptr->content)->val);
+	free(ptr->content);
+	free(ptr);
 }
