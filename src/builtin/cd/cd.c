32d04714afc9eff02498682e441a3240675119f7
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-03-15 11:14:42 +0100

    Add new file

--- src/builtin/cd/cd.c
+++ src/builtin/cd/cd.c
@@ -6,7 +6,42 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/14 15:10:22 by pmolnar       #+#    #+#                 */
-/*   Updated: 2023/03/14 17:32:27 by pmolnar       ########   odam.nl         */
+/*   Updated: 2023/03/15 11:08:57 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <minishell.h>
+
+void	cd(char *dir, t_shell_data *data)
+{
+	t_list	*home_env_var;
+	t_list	*cdpath_env_var;
+	t_list	*var;
+	char	*home_val;
+
+	home_env_var = find_var_by_name("HOME", data->env_vars);
+	cdpath_env_var = find_var_by_name("CDPATH", data->env_vars);
+	if (!dir || *dir == '\0')
+	{
+		if (home_env_var == NULL || *((t_var *) home_env_var->content)->val == '\0')
+			return ;
+		home_val = ((t_var *) home_env_var->content)->val;
+		cd (home_val, data);
+
+	}
+	else if (*dir == '/')
+	{
+		var = find_var_by_name("PWD", data->env_vars);
+		if (var)
+			((t_var *)var->content)->val = dir;
+	}
+
+	// 	if (!tmp)
+	// 		return ;
+	// 	// free tmp somehow!
+	// 	tmp = ft_strjoin(tmp, "/");
+	// 	tmp = ft_strjoin(tmp, dir);
+	// 	cd(tmp, data);
+	// }
+}
 
