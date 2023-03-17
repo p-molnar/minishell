c51113687e7b66916d4b105f270c9be00501512e
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-03-17 10:57:57 +0100

    Add condition for dir starting with `/`

--- src/builtin/cd/cd.c
+++ src/builtin/cd/cd.c
@@ -6,7 +6,7 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/14 15:10:22 by pmolnar       #+#    #+#                 */
-/*   Updated: 2023/03/15 14:47:06 by pmolnar       ########   odam.nl         */
+/*   Updated: 2023/03/17 10:31:55 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,49 @@
 #include <ms_macros.h>
 #include <libft.h>
 
+void	str_preprocess(char **dir, t_var **var)
+{
+	char	*pwd;
+	char	*tmp;
+	int		len;
+
+	pwd = var[PWD]->val;
+	len = ft_strlen(pwd);
+	tmp = pwd;
+	if (pwd[len - 1] != '/')
+	{
+		tmp = ft_strjoin(pwd, "/");
+		if (!tmp)
+			return ;
+	}
+	tmp = ft_strjoin(tmp, *dir);
+	if (!tmp)
+		return ;
+	*dir = tmp;
+}
+
 void	cd(char *dir, t_shell_data *data)
 {
-	t_var	*var[SIZE];
+	t_var	*var[ENV_SIZE];
 
+	var[HOME] = get_var("HOME", data->env_vars);
+	var[PWD] = get_var("PWD", data->env_vars);
+	var[OLDPWD] = get_var("PWD", data->env_vars);
 	if (!dir || *dir == '\0')
 	{
-		var[HOME] = get_var("HOME", data->env_vars);
-		if (var[HOME] == NULL || var[HOME]->val == '\0')
+		if (var[HOME] == NULL || *var[HOME]->val == '\0')
 			return ;
 		cd (var[HOME]->val, data);
 	}
 	else if (*dir == '/')
 	{
-		var[PWD] = get_var("PWD", data->env_vars);
 		if (var[PWD])
 			update_wdirs(ft_strdup(dir), var, data);
+	}
+	else if (*dir != '/')
+	{
+		str_preprocess(&dir, var);
+		cd(dir, data);
 	}
 }
 
