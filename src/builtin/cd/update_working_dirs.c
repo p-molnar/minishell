c1da1c9be076358997ee94a19318780458a28c82
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-03-17 10:58:57 +0100

    Bug fix: heap-use-after-free
    The function attempted to use memory that was freed by another function. The
    solution involves string duplication that will be freed when the program
    terminates.

--- src/builtin/cd/update_working_dirs.c
+++ src/builtin/cd/update_working_dirs.c
@@ -6,7 +6,7 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/15 14:21:58 by pmolnar       #+#    #+#                 */
-/*   Updated: 2023/03/15 15:19:10 by pmolnar       ########   odam.nl         */
+/*   Updated: 2023/03/17 10:06:58 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,35 +14,36 @@
 #include <ms_macros.h>
 #include <libft.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-void	update_oldpwd(t_var	**var, t_shell_data *data)
+void	update_oldpwd(t_var	*var[ENV_SIZE], t_shell_data *data)
 {
 	t_var	*new_var;
 
-	var[OLDPWD] = get_var("OLDPWD", data->env_vars);
 	if (!var[OLDPWD])
 	{
+		// printf("oldpwd not exists\n");
 		new_var = ft_calloc(1, sizeof(t_var));
 		if (!new_var)
 			return ;
-		new_var->name = "OLDPWD"; // read only issue with free
-		new_var->val = var[PWD]->val;
+		new_var->name = ft_strdup("OLDPWD"); // check for null
+		new_var->val = ft_strdup(var[PWD]->val);
 		add_var(new_var, &data->env_vars);
 	}
 	else
 	{
 		free(var[OLDPWD]->val);
-		var[OLDPWD]->val = var[PWD]->val;
+		var[OLDPWD]->val = ft_strdup(var[PWD]->val);
 	}
 }
 
-void	update_pwd(char *dir, t_var **var)
+void	update_pwd(char *dir, t_var *var[ENV_SIZE])
 {
 	free(var[PWD]->val);
 	var[PWD]->val = dir;
 }
 
-void	update_wdirs(char *dir, t_var **var, t_shell_data *data)
+void	update_wdirs(char *dir, t_var *var[ENV_SIZE], t_shell_data *data)
 {
 	update_oldpwd(var, data);
 	update_pwd(dir, var);
