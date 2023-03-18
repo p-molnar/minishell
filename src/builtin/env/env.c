a4f174f9fa4172b6f69ddfcf69a6bfd66f8a9cbd
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-03-18 19:59:14 +0100

    Modify env to only print defined vars

--- src/builtin/env/env.c
+++ src/builtin/env/env.c
@@ -6,7 +6,7 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/13 16:02:38 by pmolnar       #+#    #+#                 */
-/*   Updated: 2023/03/13 16:05:56 by pmolnar       ########   odam.nl         */
+/*   Updated: 2023/03/18 19:58:24 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,8 @@ void	env(t_list *var_list)
 	while (var_list)
 	{
 		var = var_list->content;
-		printf("%s=%s\n", var->name, var->val);
+		if (var->name && var->val)
+			printf("%s=%s\n", var->name, var->val);
 		var_list = var_list->next;
 	}
 }
