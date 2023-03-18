de04b9d1d35edaf73eb46e66fb7c8b67800fe707
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-03-18 20:13:38 +0100

    Modify `export` function for variable declaration
    So far the function was only able to set variable definitions. That is a
    key-value pair joined with an equation sign. The shell is now able to
    handle variable declarations as well whereby only a variable name is
    reserved, but no value is assigned to it.

--- src/builtin/export/export.c
+++ src/builtin/export/export.c
@@ -6,7 +6,7 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/13 13:40:23 by pmolnar       #+#    #+#                 */
-/*   Updated: 2023/03/15 11:59:15 by pmolnar       ########   odam.nl         */
+/*   Updated: 2023/03/18 20:13:16 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,27 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void	print_exported_vars(t_shell_data *data)
+{
+	(void) data;
+	return ;
+}
+
 void	export(t_token_list *token, t_shell_data *data)
 {
-	t_var	tmp_var;
 	t_var	*var;
 
-	if (is_valid_var_definition(token->content))
-		parse_var(token->content, &tmp_var);
-	else
-		tmp_var.name = token->content;
-	var = get_var(tmp_var.name, data->shell_vars);
+	var = ft_calloc(1, sizeof(t_var));
 	if (!var)
 		return ;
+	if (token == NULL)
+		print_exported_vars(data);
+	else if (is_valid_var_definition(token->content))
+		parse_var(token->content, var);
+	else
+	{
+		var->name = token->content;
+		var->val = NULL;
+	}
 	add_var(var, &data->env_vars);
 }
