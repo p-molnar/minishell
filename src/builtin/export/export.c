8ca4890308b287e5253cd6bb98f1b5a9df56160c
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-03-18 20:23:56 +0100

    Add printer functionality to `export`

--- src/builtin/export/export.c
+++ src/builtin/export/export.c
@@ -6,7 +6,7 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/13 13:40:23 by pmolnar       #+#    #+#                 */
-/*   Updated: 2023/03/18 20:13:16 by pmolnar       ########   odam.nl         */
+/*   Updated: 2023/03/18 20:22:51 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,21 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void	print_exported_vars(t_shell_data *data)
+void	print_exported_vars(t_list *var_list)
 {
-	(void) data;
+	t_var	*curr_var;
+
+	while (var_list)
+	{
+		curr_var = var_list->content;
+		printf("declare -x ");
+		if (curr_var->name && curr_var->val)
+			printf("%s=%s", curr_var->name, curr_var->val);
+		else if (curr_var->name && curr_var->val == NULL)
+			printf("%s", curr_var->name);
+		printf("\n");
+		var_list = var_list->next;
+	}
 	return ;
 }
 
@@ -28,12 +40,15 @@ void	export(t_token_list *token, t_shell_data *data)
 	if (!var)
 		return ;
 	if (token == NULL)
-		print_exported_vars(data);
+	{
+		print_exported_vars(data->env_vars);
+		return ;
+	}
 	else if (is_valid_var_definition(token->content))
 		parse_var(token->content, var);
 	else
 	{
-		var->name = token->content;
+		var->name = ft_strdup(token->content); // check for return val!
 		var->val = NULL;
 	}
 	add_var(var, &data->env_vars);
