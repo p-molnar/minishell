4f49ea62ac2d04a9f25c070872baf1b159171b3c
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-03-21 16:59:13 +0100

    Add file

---
+++ src/builtin/cd/cd_utils.c
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   cd_utils.c                                         :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/03/21 11:18:45 by pmolnar       #+#    #+#                 */
+/*   Updated: 2023/03/21 16:53:00 by pmolnar       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <minishell.h>
+#include <stdlib.h>
+
+char	*path_concat(char *basepath, char *relpath)
+{
+	int		len[2];
+	char	*joined_str;
+
+	len[0] = ft_strlen(basepath);
+	len[1] = ft_strlen(relpath);
+	if (basepath[len[0] - 1] != '/')
+	{
+		basepath = ft_strjoin(basepath, "/");
+		if (!basepath)
+			return (NULL);
+	}
+	if (relpath[len[1] - 1] != '/')
+	{
+		relpath = ft_strjoin(relpath, "/");
+		if (!relpath)
+			return (NULL);
+	}
+	joined_str = ft_strjoin(basepath, relpath);
+	if (!joined_str)
+		return (NULL);
+	return (joined_str);
+	//	proper freeing
+}
+
+int	get_arr_size(void **arr)
+{
+	int	i;
+
+	i = 0;
+	while (arr && arr[i])
+		i++;
+	return (i);
+}
+
+void	free_arr(void **arr)
+{
+	int	i;
+
+	i = 0;
+	while (arr && arr[i])
+		free (arr[i++]);
+	if (arr)
+		free (arr);
+}
+
+char	*n_arr_to_str(char **arr, char *c, int n)
+{
+	int		i;
+	char	*str;
+	char	*tmp;
+	char	*tmp2;
+
+	i = 0;
+	str = ft_strdup("");
+	while (arr && arr[i] && n)
+	{
+		if (i > 0)
+		{
+			free(tmp);
+			free(tmp2);
+		}
+		tmp = str;
+		str = ft_strjoin(tmp, c);
+		tmp2 = str;
+		str = ft_strjoin(tmp2, arr[i]);
+		i++;
+		n--;
+	}
+	if (str[0] == '\0')
+		return (NULL);
+	return (str);
+}
