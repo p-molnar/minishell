c8779e8d7cd4e7558296d2d38104946d79432c88
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-03-21 23:22:38 +0100

    Change dot-dot processing

--- src/builtin/cd/step_8.c
+++ src/builtin/cd/step_8.c
@@ -6,13 +6,14 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/21 15:28:11 by pmolnar       #+#    #+#                 */
-/*   Updated: 2023/03/21 16:58:49 by pmolnar       ########   odam.nl         */
+/*   Updated: 2023/03/21 23:21:01 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 
 /*
 8. The curpath value shall then be converted to canonical form
@@ -72,52 +73,74 @@ char	**remove_dot_comp(char **arr, int size)
 	return (new_arr);
 }
 
-char **dir_move_up(char **arr, int dd_comp_index)
+char	**dir_move_up(char **arr, int dd_comp_index)
 {
-	char	*path;
+	char	**processed_arr;
+	int		i;
+	int		j;
 
+	processed_arr = ft_calloc(get_arr_size((void **)arr), sizeof(char *));
+	if (!processed_arr)
+		return (NULL);
 	if (dd_comp_index > 0 && ft_strncmp(arr[0], "..", 3) != 0)
 	{
-		path = n_arr_to_str(arr, "/", dd_comp_index - 1);
-		printf("path: %s\n", path);
-		// if (access(path, F_OK) != -1);
+		i = 0;
+		j = 0;
+		while (arr && arr[i])
+		{
+			if (i == dd_comp_index - 1)
+			{
+				i++;
+				continue ;
+			}
+			processed_arr[j] = arr[i];
+			i++;
+			j++;
+		}
+		return (processed_arr);
 	}
 	return (NULL);
 }
 
-char **process_dotdot_comp(char **arr, int size)
+char	*process_dotdot_comp(char **arr)
 {
-	int i;
-	int	has_dotdot_comp;
+	int		i;
+	char	*path;
+	char	**path_comps;
 
 	i = 0;
-	has_dotdot_comp = 1;
-	while (has_dotdot_comp)
+	path_comps = arr;
+	while (path_comps && path_comps[i])
 	{
-		while (i < size)
+		if (ft_strncmp(path_comps[i], "..", 3) == 0)
 		{
-			if (ft_strncmp(arr[i], "..", 3) == 0)
-				dir_move_up(arr, i);
-			i++;
+			path_comps = dir_move_up(path_comps, i);
+			i = 0;
+			continue ;
 		}
-		
+		i++;
 	}
+	path = n_arr_to_str(path_comps, "/", get_arr_size((void **)path_comps));
+	printf("path: %s\n", path);
+	if (access(path, F_OK) != -1)
+		return (path);
 	return (NULL);
 }
 
 int	exec_step_8(char **curpath, int *step)
 {
 	char	**comps;
+	int		i;
 
 	comps = ft_split(*curpath, '/');
 	comps = remove_dot_comp(comps, get_arr_size((void **)comps));
-	int i = 0;
+	i = 0;
 	while (comps && comps[i])
 	{
 		printf("comps[%d]=%s\n", i, comps[i]);
 		i++;
 	}
+	*curpath = process_dotdot_comp(comps);
 	*step += 1;
-	comps = process_dotdot_comp(comps, get_arr_size((void **)comps));
 	return (0);
 }
