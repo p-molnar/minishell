94b81f775e087a004130089b5c16785caeddcbdd
Jan Zaremba <jzaremba@student.codam.nl>
2023-03-23 15:17:12 +0100

    Improve builtin detection

--- src/executor/executor/exec_builtins.c
+++ src/executor/executor/exec_builtins.c
@@ -6,7 +6,7 @@
 /*   By: jzaremba <jzaremba@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/23 13:47:18 by jzaremba      #+#    #+#                 */
-/*   Updated: 2023/03/23 14:26:19 by jzaremba      ########   odam.nl         */
+/*   Updated: 2023/03/23 15:13:20 by jzaremba      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,25 @@
 
 void	execute_builtin(char *cmd, t_shell_data *data, char **args)
 {
-	if (ft_strncmp(cmd, "export", ft_strlen("export")) == 0)
+	if (ft_strncmp(cmd, "export", ft_strlen("export") + 1) == 0)
 		exit (0);
-	else if (ft_strncmp(cmd, "unset", ft_strlen("unset")) == 0)
+	else if (ft_strncmp(cmd, "unset", ft_strlen("unset") + 1) == 0)
 		exit (0);
-	else if (ft_strncmp(cmd, "cd", ft_strlen("cd")) == 0)
+	else if (ft_strncmp(cmd, "cd", ft_strlen("cd") + 1) == 0)
 		exit (0);
-	else if (ft_strncmp(cmd, "exit", ft_strlen("exit")) == 0)
+	else if (ft_strncmp(cmd, "exit", ft_strlen("exit") + 1) == 0)
 		exit (0);
-	else if (ft_strncmp(cmd, "env", ft_strlen("env")) == 0)
+	else if (ft_strncmp(cmd, "env", ft_strlen("env") + 1) == 0)
 	{
 		env(data->env_vars);
 		exit (0);
 	}
-	else if (ft_strncmp(cmd, "pwd", ft_strlen("pwd")) == 0)
+	else if (ft_strncmp(cmd, "pwd", ft_strlen("pwd") + 1) == 0)
 	{
 		pwd(data);
 		exit (0);
 	}
-	else if (ft_strncmp(cmd, "echo", ft_strlen("echo")) == 0)
+	else if (ft_strncmp(cmd, "echo", ft_strlen("echo") + 1) == 0)
 	{
 		echo(args);
 		exit (0);
