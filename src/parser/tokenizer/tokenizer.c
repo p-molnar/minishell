5965be379c218e581d3eb10c314c6a4d3cf4cb01
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-03-28 11:35:14 +0200

    Bug fix: string overflow issue
    When the tokenizer loops until the end of a string, the `end_ptr` points
    to the null terminator. However, this pointer is incremented further for
    the next loop, and may point to an address that should not be accessed
    by the function and hence tokenizes characters that don't come from the
    prompt.
    
    This commit fixes this issue by always returning a non-null terminator,
    that is the last character of a string, right before the null
    terminator.

--- src/parser/tokenizer/tokenizer.c
+++ src/parser/tokenizer/tokenizer.c
@@ -6,7 +6,7 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/02/22 13:49:17 by pmolnar       #+#    #+#                 */
-/*   Updated: 2023/03/27 18:14:28 by pmolnar       ########   odam.nl         */
+/*   Updated: 2023/03/28 11:34:34 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,21 +25,20 @@ static void	update_prev_ptr(char **prev, char *curr, char *s)
 		*prev = curr;
 }
 
-static char	*is_token_end(char *prev, char *curr)
+static char	*is_token_end(char *prev, char *curr, char *is_quoted)
 {
-	static char	is_quoted;
 
-	if (!is_quoted && ft_strchr(OPERATORS, *prev) && *curr == *prev)
+	if (!*is_quoted && ft_strchr(OPERATORS, *prev) && *curr == *prev)
 		;
-	else if (!is_quoted && ft_strchr(OPERATORS, *prev) && *curr != *prev)
+	else if (!*is_quoted && ft_strchr(OPERATORS, *prev) && *curr != *prev)
 		return (prev);
-	else if (!is_quoted && ft_strchr(QUOTES, *curr))
-		is_quoted = *curr;
-	else if (is_quoted && ft_strchr(QUOTES, *curr) && *curr == is_quoted)
-		is_quoted = 0;
-	else if (!is_quoted && ft_strchr(OPERATORS, *curr))
+	else if (!*is_quoted && ft_strchr(QUOTES, *curr))
+		*is_quoted = *curr;
+	else if (*is_quoted && ft_strchr(QUOTES, *curr) && *curr == *is_quoted)
+		*is_quoted = 0;
+	else if (!*is_quoted && ft_strchr(OPERATORS, *curr))
 		return (prev);
-	else if (!is_quoted && *curr == ' ')
+	else if (!*is_quoted && *curr == ' ')
 		return (prev);
 	return (NULL);
 }
@@ -49,17 +48,18 @@ char	*delimit_token(char *prompt)
 	char	*curr;
 	char	*prev;
 	char	*tkn_end;
+	char	quote;
 
 	curr = prompt;
 	while (curr && *curr != '\0')
 	{
 		update_prev_ptr(&prev, curr, prompt);
-		tkn_end = is_token_end(prev, curr);
+		tkn_end = is_token_end(prev, curr, &quote);
 		if (tkn_end)
 			return (tkn_end);
 		curr++;
 	}
-	return (curr);
+	return (curr - 1);
 }
 
 t_token_list	*tokenizer(const char *prompt)
@@ -76,8 +76,6 @@ t_token_list	*tokenizer(const char *prompt)
 		if (!ft_strchr(SPACES, *start_ptr))
 		{
 			end_ptr = delimit_token(start_ptr);
-			// if (*end_ptr == '\0')
-			// 	end_ptr = start_ptr + ft_strlen(start_ptr) - 1;
 			content = ft_substr(start_ptr, 0, end_ptr - start_ptr + 1);
 			add_node_last(&tokens, new_node(content, start_ptr, UNDEFINED));
 			start_ptr += end_ptr - start_ptr;
