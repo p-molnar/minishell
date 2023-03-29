f5d15c92725d3571941bb316225243778fd7f2fb
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-03-29 18:25:23 +0200

    Remove error message

--- src/builtin/pwd/pwd.c
+++ src/builtin/pwd/pwd.c
@@ -6,7 +6,7 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/14 10:22:01 by pmolnar       #+#    #+#                 */
-/*   Updated: 2023/03/15 11:59:15 by pmolnar       ########   odam.nl         */
+/*   Updated: 2023/03/29 15:18:15 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,6 @@ void	pwd(t_shell_data *data)
 	t_var	*var;
 
 	var = get_var("PWD", data->env_vars);
-	if (var)
+	if (var && var->val)
 		printf("%s\n", var->val);
-	else
-		printf("var not found\n");
 }
