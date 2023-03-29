4f1ba7ee21d638cfd83e912970145f01e5ecf944
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-03-29 09:38:10 +0200

    Add new condition to variable validator

--- src/parser/variable_parser/variable_parser.c
+++ src/parser/variable_parser/variable_parser.c
@@ -6,7 +6,7 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/08 09:15:33 by pmolnar       #+#    #+#                 */
-/*   Updated: 2023/03/28 12:36:41 by pmolnar       ########   odam.nl         */
+/*   Updated: 2023/03/29 09:37:22 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int	is_valid_var_definition(char *s)
 	i = 0;
 	if (ft_isdigit(s[i++]))
 		return (0);
-	while (ft_isalnum(s[i]))
+	while (ft_isalnum(s[i]) || s[i] == '_')
 		i++;
 	if (s[i] != EQUAL)
 		return (0);
