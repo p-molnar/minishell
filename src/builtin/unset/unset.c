0d5b7a500de501049eb609b5abc8ff598cb46fae
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-04-04 16:34:24 +0200

    Change var name

--- src/builtin/unset/unset.c
+++ src/builtin/unset/unset.c
@@ -6,7 +6,7 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/13 16:17:39 by pmolnar       #+#    #+#                 */
-/*   Updated: 2023/03/31 18:19:19 by jzaremba      ########   odam.nl         */
+/*   Updated: 2023/04/04 15:57:58 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,16 +75,16 @@ void	builtin_unset(char **args, t_shell_data *data)
 {
 	t_list	*env_ptr;
 	t_list	*shell_ptr;
-	char	*var_name;
+	char	*name;
 
 	args++;
 	while (args && *args)
 	{
-		var_name = *args;
-		if (!var_name || !data)
+		name = *args;
+		if (!name || !data)
 			return ;
-		shell_ptr = get_node(get_var(var_name, data->shell_vars), data->shell_vars);
-		env_ptr = get_node(get_var(var_name, data->env_vars), data->env_vars);
+		shell_ptr = get_node(get_var(name, data->shell_vars), data->shell_vars);
+		env_ptr = get_node(get_var(name, data->env_vars), data->env_vars);
 		if (shell_ptr)
 			del_node(&data->shell_vars, shell_ptr);
 		if (env_ptr)
