08509f5e6c44ca2aac4fc0a9ad8ee27060ad0949
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-04-04 12:51:08 +0200

    Outsource utility functions, add freeing

--- src/builtin/cd/cd_execution_steps/step_8.c
+++ src/builtin/cd/cd_execution_steps/step_8.c
@@ -6,7 +6,7 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/21 15:28:11 by pmolnar       #+#    #+#                 */
-/*   Updated: 2023/04/03 14:14:15 by pmolnar       ########   odam.nl         */
+/*   Updated: 2023/04/04 12:39:25 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,120 +48,19 @@
 		further steps shall be taken.
 */
 
-char	**remove_dot_comp(char **arr, int size)
-{
-	char	**new_arr;
-	int		i;
-	int		j;
-
-	new_arr = ft_calloc(size + 1, sizeof(char *));
-	if (!new_arr)
-		return (NULL);
-	i = 0;
-	j = 0;
-	while (arr && arr[j])
-	{
-		if (ft_strncmp(arr[j], ".", 2) != 0)
-			new_arr[i++] = ft_strdup(arr[j]);
-		j++;
-	}
-	return (new_arr);
-}
-
-char	**dir_move_up(char **arr, int dd_comp_index)
-{
-	char	**processed_arr;
-	int		i;
-	int		j;
-
-	processed_arr = ft_calloc(get_arr_size((void **)arr), sizeof(char *));
-	if (!processed_arr)
-		return (NULL);
-	if (dd_comp_index > 0 && ft_strncmp(arr[0], "..", 3) != 0)
-	{
-		i = 0;
-		j = 0;
-		while (arr && arr[i])
-		{
-			if (i == dd_comp_index - 1)
-			{
-				i++;
-				continue ;
-			}
-			processed_arr[j] = arr[i];
-			i++;
-			j++;
-		}
-		return (processed_arr);
-	}
-	return (NULL);
-}
-
-char	**move_up_dir(char **arr, int curr_comp_idx)
-{
-	char	**new_arr;
-	int		j;
-	int		k;
-
-	new_arr = ft_calloc(get_arr_size((void **)arr) + 1, sizeof(char *));
-	if (!new_arr)
-		return (NULL);
-	j = 0;
-	k = 0;
-	while (arr && arr[j])
-	{
-		if (j == curr_comp_idx || j == curr_comp_idx - 1)
-		{
-			j++;
-			continue ;
-		}
-		new_arr[k++] = arr[j++];
-		// new_arr[k] = NULL;
-	}
-	return (new_arr);
-}
-
-char	*process_dotdot_comp(char **arr)
-{
-	int		i;
-	char	*path;
-	char	**tmp;
-
-	i = 0;
-	while (arr && arr[i])
-	{
-		if (ft_strncmp(arr[i], "..", 3) == 0)
-		{
-			path = strnjoin(arr, "/", i - 1);
-			if (access(path, F_OK) != -1)
-			{
-				tmp = arr;
-				arr = move_up_dir(tmp, i);
-				free_arr((void **) arr);
-				i = 0;
-				continue ;
-			}
-		}
-		i++;
-	}
-	path = strnjoin(arr, "/", get_arr_size((void **)arr));
-	// free_arr((void **) arr);
-	return (path);
-}
-
 int	exec_step_8(char **curpath, int *step)
 {
 	char	**comps;
 	char	**tmp;
 
-		
 	comps = ft_split(*curpath, '/');
 	tmp = comps;
+	free(*curpath);
 	comps = remove_dot_comp(comps, get_arr_size((void **)comps));
 	free_arr((void **)tmp);
 	tmp = comps;
 	*curpath = process_dotdot_comp(comps);
-	free_arr((void **) tmp);
+	free_arr((void **)tmp);
 	*step += 1;
 	if (curpath == NULL)
 		return (EXIT_FAILURE);
