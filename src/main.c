2c21fd272c5b42ba7af899d7c439b4abf5aa5173
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-04-04 16:33:06 +0200

    Refactor code

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/02/20 13:47:47 by pmolnar       #+#    #+#                 */
-/*   Updated: 2023/04/04 11:35:20 by pmolnar       ########   odam.nl         */
+/*   Updated: 2023/04/04 16:29:45 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,66 +17,6 @@
 #include <termios.h>
 #include <readline/history.h>
 
-void	print_tokens(t_token_list *list)
-{
-	int i;
-	// const char	*token[4] = {"UNDEF", "INVALID", "WORD", "OPERATOR"};
-
-	i = 0;
-	while (list)
-	{
-		// printf("#%d\t:%s: ->%s\n", i + 1, list->content, token[list->type + 1]);
-		// printf("%p\n", list);
-		printf("|%s|\n", list->content);
-		i++;
-		list = list->next;
-	}
-}
-
-void	print_variables(t_list *list, char *heading)
-{
-	t_var *var;
-	// int		i;
-
-	// i = 0;
-	(void)heading;
-	while (list)
-	{
-		// if (!i)
-		// 	printf("\n%s\n", heading);
-		var = list->content;
-		// printf("#%i\t%s=%s\n", i + 1, var->name, var->val);
-		printf("%s=%s\n", var->name, var->val);
-		list = list->next;
-		// i++;
-	}
-}
-
-void	print_commands(t_command_list *list)
-{
-	int i;
-	const char *symbol[8] = {"COMMAND", "ARGUMENT", "ASSIGNMENT", "OUTFILE",
-							 "OUTFILE_APPEND", "INFILE", "HEREDOC DELIMITER", "PIPE"};
-
-	i = 0;
-	while (list)
-	{
-		printf("#%d : -> %s\n", i + 1, symbol[list->symbol]);
-		if (list->token)
-		{
-			printf("%s\n", list->token->content);
-		}
-		i++;
-		list = list->next;
-	}
-}
-
-void	cleanup_before_exit(t_shell_data *data)
-{
-	tcsetattr(0, 0, &data->original_termios);
-	clear_history();
-}
-
 void	initialise_data(t_shell_data *data)
 {
 	data->env_vars = NULL;
@@ -85,40 +25,47 @@ void	initialise_data(t_shell_data *data)
 	data->tokens = NULL;
 	tcgetattr(0, &data->original_termios);
 	g_exit_status = 0;
+	data->commands = NULL;
+}
+
+void	parse_prompt(t_shell_data *data)
+{
+	data->tokens = tokenizer(data->prompt);
+	parse_shell_variable(data);
+	classify_tokens(data->tokens);
+	data->commands = parse_commands(data->tokens);
+}
+
+void	execute_command_list(t_shell_data *data)
+{
+	expand_tokens(data);
+	executor(data, data->commands);
+}
+
+void	ignore_fn_arg(int arg1, char **arg2)
+{
+	(void) arg1;
+	(void) arg2;
 }
 
 int	main(int argc, char *argv[], char *envp[])
 {
-	int prog_running;
-	t_shell_data data;
-	t_command_list *commands;
-	(void) argv;
-	(void) argc;
+	t_shell_data	data;
 
-	prog_running = 1;
-	commands = NULL;
+	ignore_fn_arg(argc, argv);
 	initialise_data(&data);
 	parse_env_variable(envp, &data.env_vars);
-	// print_variables(data.env_vars, "ENV VARS");
-	while (prog_running)
+	while (1)
 	{
 		setup_signal_handler(&data);
 		data.prompt = read_prompt(PROMPT_MSG);
 		if (!data.prompt)
-			break;
-		data.tokens = tokenizer(data.prompt);
-		parse_shell_variable(&data);
-		classify_tokens(data.tokens);
-		commands = parse_commands(data.tokens);
-		if (ft_strncmp(data.prompt, "set", 4) == 0)
-			print_variables(data.shell_vars, NULL);
-		if (commands)
+			break ;
+		parse_prompt(&data);
+		if (data.commands)
 		{
-			expand_tokens(&data);
-			// print_tokens(data.tokens);
-			// print_commands(commands);
-			executor(&data, commands);
-			free_command_list(&commands);
+			execute_command_list(&data);
+			free_command_list(&data.commands);
 		}
 		free_token_list(data.tokens);
 		free(data.prompt);
