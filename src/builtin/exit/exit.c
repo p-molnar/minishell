68dd7aa089f7da72ac78f72c9ef5d61a03f6a8ab
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-04-04 15:49:24 +0200

    Revert to 62ba065

--- src/builtin/exit/exit.c
+++ src/builtin/exit/exit.c
@@ -6,7 +6,7 @@
 /*   By: jzaremba <jzaremba@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/28 13:55:12 by jzaremba      #+#    #+#                 */
-/*   Updated: 2023/04/03 13:41:18 by jzaremba      ########   odam.nl         */
+/*   Updated: 2023/03/31 14:44:08 by jzaremba      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,31 +20,45 @@ int	is_number(char *arg)
 	int	i;
 
 	i = 0;
-	if (arg[i] == '-' || arg[i] == '+')
+	while (ft_isspace(arg[i]))
+		i++;
+	if (arg[i] == '-' || (arg[i] == '+'))
 		i++;
 	while (arg[i] != '\0')
 	{
-		if (!ft_isdigit(arg[i]))
+		if (!ft_isdigit(arg[i]) && !ft_isspace(arg[i]))
 			return (0);
+		if (ft_isspace(arg[i]))
+		{
+			while (ft_isspace(arg[i]))
+				i++;
+			if (arg[i] == '\0')
+				return (1);
+			else
+				return (0);
+		}
 		i++;
 	}
-	if (arg[i] == '-' || arg[i] == '+')
-		return (0);
 	return (1);
 }
 
-int	builtin_exit(char **args)
+void	builtin_exit(char **args)
 {
 	if (!args)
 		exit(0);
 	if (args[1])
 	{
 		if (!is_number(args[1]))
-			error("numeric argument required", EXIT, 255);
+		{
+			ft_putendl_fd("numeric argument required", STDERR_FILENO);
+			exit(255);
+		}
 		if (args[2])
-			return (error("too many arguments", RETURN, 1));
+		{
+			ft_putendl_fd("too many arguments", STDERR_FILENO);
+			return ;
+		}
 		exit(ft_atoi(args[1]));
 	}
 	exit(0);
-	return (EXIT_SUCCESS);
 }
