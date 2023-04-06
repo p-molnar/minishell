4995cff70f1f61f07f24dabb1943bdf7bcb3f95f
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-04-06 12:36:10 +0200

    Add explicit check for ret val

--- src/parser/character_reader/character_reader.c
+++ src/parser/character_reader/character_reader.c
@@ -6,7 +6,7 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/02/21 14:40:20 by pmolnar       #+#    #+#                 */
-/*   Updated: 2023/02/27 16:21:46 by jzaremba      ########   odam.nl         */
+/*   Updated: 2023/04/06 12:35:58 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ char	*read_prompt(char *prompt)
 	prompt_read = readline(prompt);
 	if (prompt_read)
 	{
-		if (ft_strncmp(prompt_read, "", 1))
+		if (ft_strncmp(prompt_read, "", 1) != 0)
 			add_history(prompt_read);
 	}
 	return (prompt_read);
