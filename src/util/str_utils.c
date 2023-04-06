72afaa03f000ec63eac82cd233ed453a667491df
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-04-06 16:55:08 +0200

    Add file

---
+++ src/util/str_utils.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   str_utils.c                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/04/06 13:56:43 by pmolnar       #+#    #+#                 */
+/*   Updated: 2023/04/06 16:10:04 by pmolnar       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdarg.h>
+#include <libft.h>
+#include <stdlib.h>
+#include <minishell.h>
+
+char	*nstrconcat(int n, ...)
+{
+	va_list	ap;
+	char	*tmp;
+	char	*r;
+	char	*arg;
+
+	va_start(ap, n);
+	r = NULL;
+	tmp = NULL;
+	while (n > 0)
+	{
+		arg = va_arg(ap, char *);
+		if (!arg)
+			arg = "";
+		if (!r)
+			r = ft_strjoin("", arg);
+		else
+			r = ft_strjoin(r, arg);
+		if (!r)
+		{
+			free_obj((void **)&tmp);
+			return (NULL);
+		}
+		free_obj((void **)&tmp);
+		tmp = r;
+		n--;
+	}
+	va_end(ap);
+	return (r);
+}
