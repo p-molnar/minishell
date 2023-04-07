fc4ac37c121492e675b6235fb997aca22cc23f89
Jan Zaremba <jzaremba@student.codam.nl>
2023-04-07 17:43:27 +0200

    Replace multiple ft_strjoins with strconcat

--- src/parser/command_list/parser_utils.c
+++ src/parser/command_list/parser_utils.c
@@ -6,7 +6,7 @@
 /*   By: jzaremba <jzaremba@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/03 16:57:16 by jzaremba      #+#    #+#                 */
-/*   Updated: 2023/04/04 12:17:23 by jzaremba      ########   odam.nl         */
+/*   Updated: 2023/04/07 16:57:56 by jzaremba      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,14 @@
 
 t_command_list	*syntax_error(t_command_list **list, t_token_list *token)
 {
-	char	*err_tkn;
 	char	*err_msg;
 
 	if (!token)
 		error("syntax error: unexpected end of token list", RETURN, 258);
 	else
 	{
-		err_tkn = ft_strjoin(token->content, "'");
-		err_msg = ft_strjoin("syntax error near unexpected token `", err_tkn);
+		err_msg = strconcat(3, "syntax error near unexpected token `", token->content, "'");
 		error(err_msg, RETURN, 258);
-		free(err_tkn);
 		free(err_msg);
 	}
 	free_command_list(list);
