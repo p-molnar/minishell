6110d6ee2bd4ac066069c0a8e1e3c70d180834c9
Jan Zaremba <jzaremba@student.codam.nl>
2023-04-07 17:41:45 +0200

    Add bitwise validation

--- src/parser/command_list/parse_commands.c
+++ src/parser/command_list/parse_commands.c
@@ -6,7 +6,7 @@
 /*   By: jzaremba <jzaremba@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/01 13:33:38 by jzaremba      #+#    #+#                 */
-/*   Updated: 2023/04/03 16:58:07 by jzaremba      ########   odam.nl         */
+/*   Updated: 2023/04/07 17:36:44 by jzaremba      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,14 +33,14 @@ int	add_arguments(t_command_list **command_list, t_token_list *token)
 	ret = RET_END;
 	while (token)
 	{
-		if (token->type == OPERATOR)
+		if (token->type & OPERATOR)
 		{
 			ret = parse_operator(command_list, token);
 			if (ret)
 				return (ret);
 			token = token->next;
 		}
-		else if (token->type == WORD)
+		else if (token->type & WORD)
 			add_command_back(command_list, new_command_node(ARG, token));
 		token = token->next;
 	}
@@ -56,14 +56,14 @@ int	add_simple_command(t_command_list **command_list, t_token_list *token)
 	ret = RET_END;
 	while (command_flag == 0 && token)
 	{
-		if (token->type == OPERATOR)
+		if (token->type & OPERATOR)
 		{
 			ret = parse_operator(command_list, token);
 			if (ret)
 				return (ret);
 			token = token->next;
 		}
-		else if (token->type == WORD)
+		else if (token->type & WORD)
 			command_flag = (add_command(command_list, token));
 		token = token->next;
 	}
@@ -73,9 +73,9 @@ int	add_simple_command(t_command_list **command_list, t_token_list *token)
 
 t_command_list	*check_token(t_command_list **command_list, t_token_list *token)
 {
-	if (token->type == OPERATOR && ft_strncmp(token->content, "|", 1) == 0)
+	if (token->type & OPERATOR && ft_strncmp(token->content, "|", 1) == 0)
 		return (syntax_error(command_list, token));
-	if (token->type == INVALID || token->type == UNDEFINED)
+	if (token->type & INVALID || token->type & UNDEFINED)
 		return (syntax_error(command_list, token));
 	if (add_simple_command(command_list, token) == RET_SYNTAX_ERR)
 		return (NULL);
@@ -93,13 +93,13 @@ t_command_list	*parse_commands(t_token_list *token)
 			return (NULL);
 		while (token->next)
 		{
-			if (!(token->type == OPERATOR
+			if (!(token->type & OPERATOR
 					&& ft_strncmp(token->content, "|", 1) == 0))
 				token = token->next;
 			else
 				break ;
 		}
-		if (token->type == OPERATOR && ft_strncmp(token->content, "|", 1) == 0)
+		if (token->type & OPERATOR && ft_strncmp(token->content, "|", 1) == 0)
 		{
 			add_command_back(&command_list, new_command_node(D_PIPE, NULL));
 			if (!token->next)
