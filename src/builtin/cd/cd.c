606461674bc31bb8b099b0666376b91fa7ac080d
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-04-10 12:55:40 +0200

    Add correction
    `cd` removed any trailing slashes from the path. The current fix adds
    back the trailing character.

--- src/builtin/cd/cd.c
+++ src/builtin/cd/cd.c
@@ -6,7 +6,7 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/14 15:10:22 by pmolnar       #+#    #+#                 */
-/*   Updated: 2023/04/10 10:43:31 by pmolnar       ########   odam.nl         */
+/*   Updated: 2023/04/10 12:19:58 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ static void	init_env_vars(t_var **env_var, t_list *var_list)
 {
 	env_var[HOME] = get_var("HOME", var_list, ENV);
 	env_var[OLDPWD] = get_var("OLDPWD", var_list, ENV);
-	env_var[CDPATH] = get_var("CDPATH", var_list, ENV);
+	env_var[ITERABLE_PATH] = get_var("CDPATH", var_list, ENV);
 	env_var[PWD] = create_var(ft_strdup("PWD"),
 			getcwd(NULL, 0),
 			UNDEFINED);
@@ -29,6 +29,8 @@ static void	init_env_vars(t_var **env_var, t_list *var_list)
 // https://man7.org/linux/man-pages/man1/cd.1p.html
 int	get_abs_path(char *dir, char **curpath, t_var **env_var, int step)
 {
+	char	*tmp;
+
 	if ((step == 1 || step == 2) && exec_step_1_2(env_var, &dir, &step))
 		return (EXIT_FAILURE);
 	if (step == 3)
@@ -43,6 +45,11 @@ int	get_abs_path(char *dir, char **curpath, t_var **env_var, int step)
 		exec_step_7(curpath, env_var, &step);
 	if (step == 8)
 		exec_step_8(curpath, &step);
+	if (dir && dir[ft_strlen(dir) - 1] != '/' && curpath)
+		return (EXIT_SUCCESS);
+	tmp = *curpath;
+	*curpath = strconcat(2, *curpath, "/");
+	free_obj((void **)&tmp);
 	return (EXIT_SUCCESS);
 }
 
@@ -58,7 +65,7 @@ int	builtin_cd(char **args, t_shell_data *data)
 	init_env_vars(env_var, data->variables);
 	if (get_abs_path(args[1], &curpath, env_var, start_step))
 		return (EXIT_FAILURE);
-	ret = update_wdirs(curpath, env_var, data);
+	ret = update_wdirs(curpath, args[1], env_var, data);
 	free (curpath);
 	free_var(&env_var[PWD]);
 	return (ret);
