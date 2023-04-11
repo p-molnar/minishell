05681a8b03b0d1dccbc997285e8aef19b9b97080
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2023-04-11 10:26:23 +0200

    Add NULL resetter

--- src/util/free_objects_2.c
+++ src/util/free_objects_2.c
@@ -6,7 +6,7 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/04 16:04:17 by pmolnar       #+#    #+#                 */
-/*   Updated: 2023/04/06 14:38:02 by pmolnar       ########   odam.nl         */
+/*   Updated: 2023/04/11 10:12:10 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,9 +38,16 @@ void	free_arr(void **arr)
 
 	i = 0;
 	while (arr && arr[i])
-		free (arr[i++]);
+	{
+		free (arr[i]);
+		arr[i] = NULL;
+		i++;
+	}
 	if (arr)
+	{
 		free (arr);
+		arr = NULL;
+	}	
 }
 
 void	free_obj(void **obj)
